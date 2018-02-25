bbabe943536d217e5f17bbe773a421536a7430fd
ssauuser <61082015@mail.ru>
2018-02-25 16:14:32 +0300

    Кабинеты клиента и рабочего

---
+++ client/src/app/client.ts
@@ -0,0 +1,11 @@
+import {Car} from './car';
+import {Order} from './order';
+import {AvailableOrder} from './available-order';
+
+export class Client {
+  constructor( public id: number, public login: string, public password: string, public name: string, public fname: string,
+               public phone_number: number, public city: string, public rating: number,
+               public carSet: Car[] = null, public orderSet: Order[] = null,
+               public availableOrderSet: AvailableOrder[] = null) {
+  }
+}
