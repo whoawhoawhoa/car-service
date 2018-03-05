94977849a4bf7d02f3f0b7ea4c4253556aa218e1
ssauuser <61082015@mail.ru>
2018-03-05 17:23:15 +0300

    Мелкие фиксы

--- client/src/app/app-routing/app-routing.module.ts
+++ client/src/app/app-routing/app-routing.module.ts
@@ -24,7 +24,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'worker-auth', pathMatch: 'full'}
 
 ];
-
+//wtf
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
   exports: [ RouterModule ]
